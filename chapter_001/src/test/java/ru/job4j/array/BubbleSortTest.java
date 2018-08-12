aebafba691a24eb614bd6bf452c195bc50b9824e
AnatoliyEzhemenskiy <mailto:a.ezhem@gmail.com>
2018-08-12 14:04:36 +0300

    6.6. Двухмерный массив. Таблица умножения.

--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -8,7 +8,7 @@ public class BubbleSortTest {
     @Test
     public void when5319then1359() {
         int[] arr = new int[]{5, 3, 1, 9};
-        int[] expect = new int[] {1, 3, 5, 9};
+        int[] expect = new int[]{1, 3, 5, 9};
 		BubbleSort bsort = new BubbleSort();
         arr = bsort.sort(arr);
         assertThat(arr, is(expect));

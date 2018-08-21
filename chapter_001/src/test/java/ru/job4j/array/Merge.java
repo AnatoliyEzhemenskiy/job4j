a66466c837059b1c5ab750d8ed67c219a470cf12
AnatoliyEzhemenskiy <mailto:a.ezhem@gmail.com>
2018-08-21 08:48:56 +0300

    7. объединить два отсортированных массива в один отсортированный

--- chapter_001/src/test/java/ru/job4j/array/Merge.java
+++ chapter_001/src/test/java/ru/job4j/array/Merge.java
@@ -10,15 +10,4 @@ public class Merge {
             result[--z] = (j < 0)||(array1[i] > array2[j]) ? array1[i--] : array2[j--];
         return result;
     }
-
-    public static void main(String[] args) {
-        Merge merge = new Merge();
-        int[] arr1 = new int[]{1,2,8,9,10};
-        int[] arr2 = new int[]{4,5,6,7};
-        int[] arr3 = merge.merge(arr1,arr2);
-
-        for(int arr: arr3){
-            System.out.println(arr);
-        }
-    }
 }

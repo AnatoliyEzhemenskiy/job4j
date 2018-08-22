a31501df9c9f4114ca78e6a4acc42145a62b1e2c
AnatoliyEzhemenskiy <mailto:a.ezhem@gmail.com>
2018-08-22 21:59:26 +0300

    7. объединить два отсортированных массива в один отсортированный

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,13 +1,13 @@
 package ru.job4j.array;
-
 public class Merge {
     public int[] merge(int[] array1, int[] array2) {
         int[] result = new int[array1.length + array2.length];
         int z = result.length;
         int i = array1.length - 1;
         int j = array2.length - 1;
-        while (z > 0)
-            result[--z] = (j < 0)||(array1[i] > array2[j]) ? array1[i--] : array2[j--];
+        while (z > 0) {
+            result[--z] = (j < 0) || (array1[i] > array2[j]) ? array1[i--] : array2[j--];
+        }
         return result;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/array/MergeTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+public class MergeTest {
+    @Test
+    public void whenTwoMergedSortedArraysThenOneBigSortedArray() {
+        Merge merge = new Merge();
+        int[] array1 = {1, 2, 4, 8, 10};
+        int[] array2 = {3, 5, 6, 7, 9};
+        int[] array3 = merge.merge(array1, array2);
+        int[] result = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(array3, is(result));
+    }
+}

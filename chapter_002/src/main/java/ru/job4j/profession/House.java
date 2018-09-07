5cb38dc63fd0f64e7b6444d4cd60f72865783e1d
AnatoliyEzhemenskiy <mailto:a.ezhem@gmail.com>
2018-09-07 15:52:06 +0300

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/profession/House.java
@@ -0,0 +1,9 @@
+package ru.job4j.profession;
+
+public class House {
+    private String description;
+
+    public House(String description) {
+        this.description = description;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,15 @@
+package ru.job4j.profession;
+/**
+ *
+ * Profession
+ *
+ * @author - AnatoliyEzhemenskiy
+ *
+ */
+public class Profession {
+    private String name;
+
+    public Profession(String name) {
+        this.name= name;
+    }
+}

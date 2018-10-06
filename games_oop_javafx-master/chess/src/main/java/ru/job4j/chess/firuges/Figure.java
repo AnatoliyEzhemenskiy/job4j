cb8f81ad78180150df63c659dec262c04a22b4cd
AnatoliyEzhemenskiy <mailto:a.ezhem@gmail.com>
2018-10-06 15:16:01 +0300

    Каркас шахматной доски

--- games_oop_javafx-master/chess/src/main/java/ru/job4j/chess/firuges/Figure.java
+++ games_oop_javafx-master/chess/src/main/java/ru/job4j/chess/firuges/Figure.java
@@ -23,11 +23,8 @@ public abstract class Figure {
      * @return boolean
      */
     protected boolean IsDiagonal (Cell source, Cell dest) {
-        int size = Math.abs(source.x - dest.x);
-        return ((source.x + source.y == dest.x + dest.y) ||
-                (source.x == dest.x - size && source.y == dest.y - size) ||
-                (source.x == dest.x + size && source.y == dest.y + size)
-        );
+        return (source.x + source.y == dest.x + dest.y) ||
+                (source.x - source.y == dest.x - dest.y);
     }
 
     /**
@@ -40,10 +37,10 @@ public abstract class Figure {
      */
     protected Cell[] getBishopSteps(Cell source, Cell dest) {
         Cell[] steps = new Cell[0];
-        int size = Math.abs(source.x - dest.x);
         if (IsDiagonal(source, dest)) {
             int deltaX = source.x > dest.x ? -1 : 1;
             int deltaY = source.y > dest.y ? -1 : 1;
+            int size = (dest.x - source.x) * deltaX;
             steps = new Cell[size];
             for (int i = 0; i < size; i++) {
                 int x = source.x + (i + 1) * deltaX;
